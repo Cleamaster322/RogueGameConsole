56b870837f321385d055f5cb614afd14482114f5
Cleamaster322 <82811442+Cleamaster322@users.noreply.github.com>
2023-03-13 16:38:13 +1000

    test v2

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Красный");
+      Console.WriteLine("Оранжевый");
+      Console.WriteLine("Желтый");
+      Console.WriteLine("Зеленый");
+      Console.WriteLine("Голубой");
+      Console.WriteLine("Синий");
+      Console.WriteLine("Фиолетовый");
+    }
+}
