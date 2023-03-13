5853571f338eda2f74e52d48e959b22dcfc24588
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2023-03-13 16:47:26 +1000

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,10 +6,5 @@ class Program
     {
       Console.WriteLine("Красный");
       Console.WriteLine("Оранжевый");
-      Console.WriteLine("Желтый");
-      Console.WriteLine("Розовый");
-      Console.WriteLine("Голубой");
-      Console.WriteLine("Синий");
-      Console.WriteLine("Белый");
     }
 }
