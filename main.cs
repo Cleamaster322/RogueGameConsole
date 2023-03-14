77d5a6b230ce2b06502809a92bc024a1560fd877
Zia-11 <zakharchuk.ial@students.dvfu.ru>
2023-03-14 10:14:36 +1000

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,10 +1,24 @@
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-      Console.WriteLine("Красный");
-      Console.WriteLine("Оранжевый");
-    }
+
+class Character{
+  public int health;
+  public int experience;
+  public int attack;
+  private string name;
+
+  public Character(int health, int experience, int attack, string name){
+    this.health = health;
+    this.experience = experience;
+    this.attack = attack;
+    this.name = name;
+  }
+}
+
+class Hero{
+
+}
+
+class Monster{
+  
 }
